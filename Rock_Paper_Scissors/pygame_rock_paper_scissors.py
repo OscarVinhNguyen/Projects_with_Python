e8e8ee156107ff0799f45a37a6c852d8d05bd39e
Oscar Nguyen <oscarnguyen@pdx.edu>
2020-06-23 01:47:03 -0700

    Update 1.0.0

--- Rock_Paper_Scissors/pygame_rock_paper_scissors.py
+++ Rock_Paper_Scissors/pygame_rock_paper_scissors.py
@@ -9,7 +9,9 @@ Made by Oscar-Vinh Nguyen
 import pygame
 import time
 
-win = pygame.display.set_mode((1280, 756))
+win_w = 1280
+win_h = 756
+win = pygame.display.set_mode((win_w, win_h))
 
 pygame.display.set_caption("Rock, Paper, Scissors, Shoot")
 
@@ -25,8 +27,8 @@ class Player():
 		self.y = y
 		self. spd = spd
 
-player_1 = Player(mode='player', color=(50, 50, 250), x=50, y=50, spd=10)
-player_2 = Player(mode='player', color=(50, 175, 50), x=1280-100, y=756-100, spd=10)
+player_1 = Player(mode='player', color=(50, 50, 250), x=50, y=win_h/2-25, spd=5)
+player_2 = Player(mode='player', color=(50, 175, 50), x=win_w-100, y=win_h/2-25, spd=5)
 
 run = True
 while run:
@@ -38,26 +40,26 @@ while run:
 	pygame.draw.rect(win, (255, 255, 255), (player_1.x+10, player_1.y+10, 30, 30))
 
 	keys = pygame.key.get_pressed()
-	if keys[pygame.K_a]:
+	if keys[pygame.K_a] and player_1.x > player_1.spd:
 		player_1.x -= player_1.spd
-	if keys[pygame.K_d]:
+	if keys[pygame.K_d] and player_1.x < win_w - 50 - player_1.spd:
 		player_1.x += player_1.spd
-	if keys[pygame.K_w]:
+	if keys[pygame.K_w] and player_1.y > player_1.spd:
 		player_1.y -= player_1.spd
-	if keys[pygame.K_s]:
+	if keys[pygame.K_s] and player_1.y < win_h - 50 - player_1.spd:
 		player_1.y += player_1.spd
 
 	pygame.draw.rect(win, player_2.color, (player_2.x, player_2.y, 50, 50))
 	pygame.draw.rect(win, (255, 255, 255), (player_2.x+10, player_2.y+10, 30, 30))
  
 	keys = pygame.key.get_pressed()
-	if keys[pygame.K_LEFT]:
+	if keys[pygame.K_LEFT] and player_2.x > player_2.spd:
 		player_2.x -= player_2.spd
-	if keys[pygame.K_RIGHT]:
+	if keys[pygame.K_RIGHT] and player_2.x < win_w - 50 - player_2.spd:
 		player_2.x += player_2.spd
-	if keys[pygame.K_UP]:
+	if keys[pygame.K_UP] and player_2.y > player_2.spd:
 		player_2.y -= player_2.spd
-	if keys[pygame.K_DOWN]:
+	if keys[pygame.K_DOWN] and player_2.y < win_h - 50 - player_2.spd:
 		player_2.y += player_2.spd
 
 	pygame.display.update()
