8884a6105d1e2dd73e64ce4bff813cfbd64d0cff
Oscar Nguyen <oscarnguyen@pdx.edu>
2020-06-26 18:57:20 -0700

    Projectile Update 1.2.1

--- Rock_Paper_Scissors/pygame_rock_paper_scissors.py
+++ Rock_Paper_Scissors/pygame_rock_paper_scissors.py
@@ -76,8 +76,11 @@ index_update = 5 # update== sprite every 6 frames (out of 60 FPS (10 sprite fram
 player_1_shoot_timer = 0
 player_2_shoot_timer = 0
 
-player_1 = Player(mode='player', color=(50, 50, 250), x=50, y=win_h/2-25, spd=5, cooldown = 30)
-player_2 = Player(mode='player', color=(50, 175, 50), x=win_w-100, y=win_h/2-25, spd=5, cooldown = 30)
+# projectile speed for all players
+proj_spd = 10
+
+player_1 = Player(mode='player', color=(50, 50, 250), x=50, y=win_h/2-25, spd=5, cooldown=90)
+player_2 = Player(mode='player', color=(50, 175, 50), x=win_w-100, y=win_h/2-25, spd=5, cooldown=90)
 
 player_1_has_control = True
 player_2_has_control = True
@@ -105,6 +108,7 @@ spr2_proj = SpriteSheet(spr2_proj_flipped, 3, 1)
 # bullet object list
 bullets = []
 
+# main run loop thingy
 run = True
 while run:
     clock.tick(FPS)
@@ -151,13 +155,13 @@ while run:
         # shooting
         if player_1_shoot_timer <= 0:
             if keys[pygame.K_f]:
-                bullets.append(Projectile(player_1.x+16, player_1.y+16, 1, 10, variety=0)) # rock
+                bullets.append(Projectile(player_1.x+16, player_1.y+16, 1, proj_spd, variety=0)) # rock
                 player_1_shoot_timer = player_1.cooldown
             if keys[pygame.K_g]:
-                bullets.append(Projectile(player_1.x+16, player_1.y+16, 1, 10, variety=1)) # paper
+                bullets.append(Projectile(player_1.x+16, player_1.y+16, 1, proj_spd, variety=1)) # paper
                 player_1_shoot_timer = player_1.cooldown
             if keys[pygame.K_h]:
-                bullets.append(Projectile(player_1.x+16, player_1.y+16, 1, 10, variety=2)) # scissors
+                bullets.append(Projectile(player_1.x+16, player_1.y+16, 1, proj_spd, variety=2)) # scissors
                 player_1_shoot_timer = player_1.cooldown
 
         # movement
@@ -176,13 +180,13 @@ while run:
         # shooting
         if player_2_shoot_timer <= 0:
             if keys[pygame.K_RSHIFT]:
-                bullets.append(Projectile(player_2.x-16, player_2.y+16, -1, 10, variety=2)) # rock
+                bullets.append(Projectile(player_2.x-16, player_2.y+16, -1, proj_spd, variety=2)) # rock
                 player_2_shoot_timer = player_2.cooldown
             if keys[pygame.K_RETURN]:
-                bullets.append(Projectile(player_2.x-16, player_2.y+16, -1, 10, variety=1)) # paper
+                bullets.append(Projectile(player_2.x-16, player_2.y+16, -1, proj_spd, variety=1)) # paper
                 player_2_shoot_timer = player_2.cooldown
             if keys[pygame.K_BACKSLASH]:
-                bullets.append(Projectile(player_2.x-16, player_2.y+16, -1, 10, variety=0)) # scissors
+                bullets.append(Projectile(player_2.x-16, player_2.y+16, -1, proj_spd, variety=0)) # scissors
                 player_2_shoot_timer = player_2.cooldown
 
         # movement
